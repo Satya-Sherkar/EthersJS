af1654a5a6c8e9aafa8f18fec432ae60b3cad8c1
satyam <satyasherkar@gmail.com>
2025-07-23 05:24:52 +0000

    update

--- src/get_balance_infura.js
+++ src/get_balance_infura.js
@@ -1,7 +1,11 @@
+/* The function of this code is to read the balance of a random address
+ from etherscan using Infura's node service. */
+
+
 // Import everything from ethersJS library
 import { ethers, formatEther } from "ethers";
 import dotenv from "dotenv";
-dotenv.config({quiet:true});
+dotenv.config({ quiet: true });
 
 // Coonecting to ethereum
 const url = `https://mainnet.infura.io/v3/${process.env.API_KEY}`;
@@ -9,7 +13,7 @@ const provider = new ethers.JsonRpcProvider(url);
 
 // check balance 
 const ADDRESS = "0x4838b106fce9647bdf1e7877bf73ce8b0bad5f97"; // Random address from etherscan
-const balance = await provider.getBalance(ADDRESS); 
+const balance = await provider.getBalance(ADDRESS);
 
 // log balance 
 console.log(`\nBalance of ${ADDRESS} is ${formatEther(balance)} ETH\n`);
